a09d4c649da5421c44dbe0d4775665efd91cd6b6
Brett <bsbustamantez@gmail.co>
2025-09-19 16:12:28 +0300

    first commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+
+nombre = "Brett" #tipo de dato string
+edad = 34 #tipo de dato entero
+altura = 1.70 #tipo de dato de dato decimal Float
+Casado = True #tipo de dato booleano
+miConjunto = {1,2,3,4,5} #tipo de dato conjunto tambien conocido como set
+milista = ["Brett","Saul","Bustamante","Zambrano"] #tipo de dato lista
+milista2 = ["Brett",34,1.70,"Zambrano",Casado] #tipo de dato lista con elementos de diferentes tipo, se puede modificar agregar, eliminar o actualizar registros una vez creada
+mitupla = ("Brett",34,1.70,"Zambrano",Casado) #tipo de dato tupla que es como una lista pero no se puede modificar una vez creada
+nada = None #tipo de dato que arroja None
+miDiccionario = {"House":"Casa","Edad":22,"conHijos":True} #tipo de dato Diccionario
+
+print(miDiccionario["House"])
