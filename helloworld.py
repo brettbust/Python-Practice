7ee5e4ddbe012049224d6b1a144818bc5ba9415e
Brett Bustamante <bsbustamantez@gmail.com>
2025-09-19 16:18:24 +0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,12 @@
 
-nombre = "Brett" #tipo de dato string
-edad = 34 #tipo de dato entero
-altura = 1.70 #tipo de dato de dato decimal Float
-Casado = True #tipo de dato booleano
+nombre = "Carlos" #tipo de dato string
+edad = 30 #tipo de dato entero
+altura = 1.90 #tipo de dato de dato decimal Float
+Casado = False #tipo de dato booleano
 miConjunto = {1,2,3,4,5} #tipo de dato conjunto tambien conocido como set
-milista = ["Brett","Saul","Bustamante","Zambrano"] #tipo de dato lista
-milista2 = ["Brett",34,1.70,"Zambrano",Casado] #tipo de dato lista con elementos de diferentes tipo, se puede modificar agregar, eliminar o actualizar registros una vez creada
-mitupla = ("Brett",34,1.70,"Zambrano",Casado) #tipo de dato tupla que es como una lista pero no se puede modificar una vez creada
+milista = ["Carlos","Jose","Ramirez","Ponte"] #tipo de dato lista
+milista2 = ["Carlos",30,1.90,"Ramirez",Soltero] #tipo de dato lista con elementos de diferentes tipo, se puede modificar agregar, eliminar o actualizar registros una vez creada
+mitupla = ("Brett",34,1.70,"Zambrano",Soltero) #tipo de dato tupla que es como una lista pero no se puede modificar una vez creada
 nada = None #tipo de dato que arroja None
 miDiccionario = {"House":"Casa","Edad":22,"conHijos":True} #tipo de dato Diccionario
 
