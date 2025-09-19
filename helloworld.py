047fd97fc033ca213e2af7a5c8ae34a531f33b01
Brett <bsbustamantez@gmail.co>
2025-09-19 16:21:58 +0300

    My secod commit

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ Casado = False #tipo de dato booleano
 miConjunto = {1,2,3,4,5} #tipo de dato conjunto tambien conocido como set
 milista = ["Carlos","Jose","Ramirez","Ponte"] #tipo de dato lista
 milista2 = ["Carlos",30,1.90,"Ramirez",Soltero] #tipo de dato lista con elementos de diferentes tipo, se puede modificar agregar, eliminar o actualizar registros una vez creada
-mitupla = ("Brett",34,1.70,"Zambrano",Soltero) #tipo de dato tupla que es como una lista pero no se puede modificar una vez creada
+mitupla = ("Carlos",34,1.70,"Ramirez",Soltero) #tipo de dato tupla que es como una lista pero no se puede modificar una vez creada
 nada = None #tipo de dato que arroja None
 miDiccionario = {"House":"Casa","Edad":22,"conHijos":True} #tipo de dato Diccionario
 
